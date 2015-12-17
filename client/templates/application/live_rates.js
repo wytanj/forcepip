5525746a5f4901294b99177ad4311cbfcb4ad824
wytanj <wytanj@gmail.com>
2015-12-18 01:30:44 +0800

    partially complete
    live_rates.js - line 13 original value is
    OANDA.rate.quote([$("#selectSymbol").val()], function(rateQuoteResponse)
    {

--- client/templates/application/live_rates.js
+++ client/templates/application/live_rates.js
@@ -1,40 +1,40 @@
-// $(function() {
-//     var selectSymbol = $("#selectSymbol");
-//     var oldBid = null;
-//     //reset the display when the symbol is changed
-//     selectSymbol.change(function() {
-//         $("#bid").text("");
-//         $("#ask").text("");
-//         oldBid = null;
-//     });
-//     var sessionToken = null;
-//     //called every second to refresh the display
-//     function getCurrentRates() {
-//         OANDA.rate.quote([$("#selectSymbol").val()], function(rateQuoteResponse) {
-//             var priceInfo = rateQuoteResponse.prices[0];
-//             //change the divs' background colour if the rates have changed. TODO: some jQuery animation would be nice:)
-//             if(oldBid && priceInfo.bid < oldBid) {
-//                 $("#quote-panel").css("background-color", "#dd0000");
-//             } else if(oldBid && priceInfo.bid > oldBid) {
-//                 $("#quote-panel").css("background-color", "#00dd00");
-//             } else {
-//                 $("#quote-panel").css("background-color", "#ffffff");
-//             }
-//             oldBid = priceInfo.bid;
-//             //update the displayed rates
-//             $("#bid").text(priceInfo.bid);
-//             $("#ask").text(priceInfo.ask);
-//         });
-//     }
-//     //get the symbol list
-//     OANDA.rate.instruments(0, ['pip', 'precision', 'marginRate'],function(listSymbolsResponse) {
-//         //add the symbols to our drop down
-//         for(var cur in listSymbolsResponse.instruments) {
-//             var symbolName = listSymbolsResponse.instruments[cur].instrument;
-//             selectSymbol.append("<option value='" + symbolName + "'>" + symbolName + "</option>");
-//         }
-//         //select EUR/USD by default
-//         selectSymbol.find("[value=EUR_USD]").attr("selected", "selected");
-//         setInterval(getCurrentRates, 1000);
-//     });
-// });
+$(function() {
+    var selectSymbol = $("#selectSymbol");
+    var oldBid = null;
+    //reset the display when the symbol is changed
+    selectSymbol.change(function() {
+        $("#bid").text("");
+        $("#ask").text("");
+        oldBid = null;
+    });
+    var sessionToken = null;
+    //called every second to refresh the display
+    function getCurrentRates() {
+        OANDA.rate.quote(["EUR_USD"], function(rateQuoteResponse) {
+            var priceInfo = rateQuoteResponse.prices[0];
+            //change the divs' background colour if the rates have changed. TODO: some jQuery animation would be nice:)
+            if(oldBid && priceInfo.bid < oldBid) {
+                $("#quote-panel").css("background-color", "#dd0000");
+            } else if(oldBid && priceInfo.bid > oldBid) {
+                $("#quote-panel").css("background-color", "#00dd00");
+            } else {
+                $("#quote-panel").css("background-color", "#ffffff");
+            }
+            oldBid = priceInfo.bid;
+            //update the displayed rates
+            $("#bid").text(priceInfo.bid);
+            $("#ask").text(priceInfo.ask);
+        });
+    }
+    //get the symbol list
+    OANDA.rate.instruments(0, ['pip', 'precision', 'marginRate'],function(listSymbolsResponse) {
+        //add the symbols to our drop down
+        for(var cur in listSymbolsResponse.instruments) {
+            var symbolName = listSymbolsResponse.instruments[cur].instrument;
+            selectSymbol.append("<option value='" + symbolName + "'>" + symbolName + "</option>");
+        }
+        //select EUR/USD by default
+        selectSymbol.find("[value=EUR_USD]").attr("selected", "selected");
+        setInterval(getCurrentRates, 1000);
+    });
+});
